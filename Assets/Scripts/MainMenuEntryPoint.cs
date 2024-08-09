0c6bee6d98015354c8c350e12479cff4d136e057
Danylo <dbashinskiy@miem.hse.ru>
2024-08-09 07:17:47 +0300

    Task#2 completed

--- Assets/Scripts/MainMenuEntryPoint.cs
+++ Assets/Scripts/MainMenuEntryPoint.cs
@@ -1,20 +1,82 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using System.Threading;
+using System.Threading.Tasks;
+using TMPro;
 
 public class MainMenuEntryPoint : MonoBehaviour
 {
-    [SerializeField] private WebLoader _webLoader;
+    [SerializeField] private GameObject _loadingPanel;
+    [SerializeField] private TMP_Text _text;
+    
+    [SerializeField] private Slider _slider; 
+    
+    [SerializeField] private int _loadingDuration; // seconds
+    [FormerlySerializedAs("_loadingDelay")] [SerializeField] private int _loadingPeriod; // milliseconds
 
-    public async void Start()
+    private readonly int _clicksThreshold = 3;
+    private int _clicksDone = 0;
+    
+    private CancellationTokenSource _ctr;
+    
+    private async void Start()
     {
+        _ctr = new CancellationTokenSource();
+        _loadingPanel.SetActive(true);
         await Boot();
     }
 
+    public void HandleButtonClick()
+    {
+        if (++_clicksDone == _clicksThreshold)
+        {
+            _ctr.Cancel();
+        }
+    }
+    
     private async Task Boot()
     {
-        var tasks = new List<Task>(3);
+        var ct = _ctr.Token;
+        await LoadTextAsync(ct);
+        if (ct.IsCancellationRequested)
+        {
+            Debug.Log("Загрузка прервана!");
+            _text.text = _text.text.Insert(4, "НЕ ");
+        }
+        else
+        {
+            Debug.Log("Загрузка завершена!");
+        }
+        _loadingPanel.SetActive(false);
+    }
 
-        await Task.WhenAll(tasks);
+    private async Task LoadTextAsync(CancellationToken ct)
+    {
+        await ImmitateTextLoading(_loadingDuration, _loadingPeriod, ct);
+    }
+
+    /// <param name="duration"> Длительность загружки, секунды. </param>
+    /// <param name="delay"> Длительность периода ожидания в загрузке, миллисекунды. </param>
+    private async Task ImmitateTextLoading(int durationS, int periodMS, CancellationToken ct)
+    {
+        float progress = 0;
+        float delta = _slider.maxValue / (durationS * 1000f / periodMS); 
+
+        while (progress < _slider.maxValue)
+        {
+            if (ct.IsCancellationRequested)
+            {
+                _slider.value = _slider.maxValue; 
+                return;
+            }
+            
+            progress += delta;
+            _slider.value = progress;
+
+            await Task.Delay(periodMS);
+        }
+        
+        _slider.value = _slider.maxValue; 
     }
 }
